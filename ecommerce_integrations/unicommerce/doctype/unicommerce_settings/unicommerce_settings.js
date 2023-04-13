42c8ab939303adf3f6da1801862b63e880ea04e1
sonali@8848digital.com <sonali@8848digital.com>
2023-04-13 17:48:25 +0530

    hide sync now button

--- ecommerce_integrations/unicommerce/doctype/unicommerce_settings/unicommerce_settings.js
+++ ecommerce_integrations/unicommerce/doctype/unicommerce_settings/unicommerce_settings.js
@@ -10,29 +10,29 @@ frappe.ui.form.on("Unicommerce Settings", {
 		frm.add_custom_button(__("View Logs"), () => {
 			frappe.set_route("List", "Ecommerce Integration Log", {"integration": "Unicommerce"});
 		});
-
+	
 		let sync_buttons = ["Items", "Orders", "Inventory"];
-
-		sync_buttons.forEach((action) => {
-			frm.add_custom_button(
-				action,
-				() => {
-					frappe.call({
-						method:
-							"ecommerce_integrations.unicommerce.utils.force_sync",
-						args: {
-							document: action,
-						},
-						callback: (r) => {
-							if (!r.exc) {
-								frappe.msgprint(__(`Intiated ${action} Sync.`));
-							}
-						},
-					});
-				},
-				__("Sync Now")
-			);
-		});
+		
+		//sync_buttons.forEach((action) => {
+       		//	frm.add_custom_button(
+		//		action,
+		//		() => {
+		//			frappe.call({
+		//				method:
+		//					"ecommerce_integrations.unicommerce.utils.force_sync",
+	        //				args: {
+		//					document: action,
+		//				},
+		//			callback: (r) => {
+		//					if (!r.exc) {
+		//					frappe.msgprint(__(`Intiated ${action} Sync.`));
+		//					}
+		//				},
+		//			});
+		//		},
+		//		__("Sync Now")
+		//	);
+		//});
 	},
 
 	onload: function (frm) {
