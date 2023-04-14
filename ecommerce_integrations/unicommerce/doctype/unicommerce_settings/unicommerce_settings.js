759f6634962a4f106025f07eb460a7d12877e2e8
sonali@8848digital.com <sonali@8848digital.com>
2023-04-14 15:25:56 +0530

    order sync button

--- ecommerce_integrations/unicommerce/doctype/unicommerce_settings/unicommerce_settings.js
+++ ecommerce_integrations/unicommerce/doctype/unicommerce_settings/unicommerce_settings.js
@@ -13,26 +13,26 @@ frappe.ui.form.on("Unicommerce Settings", {
 	
 		let sync_buttons = ["Items", "Orders", "Inventory"];
 		
-		//sync_buttons.forEach((action) => {
-       		//	frm.add_custom_button(
-		//		action,
-		//		() => {
-		//			frappe.call({
-		//				method:
-		//					"ecommerce_integrations.unicommerce.utils.force_sync",
-	        //				args: {
-		//					document: action,
-		//				},
-		//			callback: (r) => {
-		//					if (!r.exc) {
-		//					frappe.msgprint(__(`Intiated ${action} Sync.`));
-		//					}
-		//				},
-		//			});
-		//		},
-		//		__("Sync Now")
-		//	);
-		//});
+		sync_buttons.forEach((action) => {
+       			frm.add_custom_button(
+				action,
+				() => {
+				frappe.call({
+						method:
+							"ecommerce_integrations.unicommerce.utils.force_sync",
+	        				args: {
+							document: action,
+						},
+					callback: (r) => {
+							if (!r.exc) {
+							frappe.msgprint(__(`Intiated ${action} Sync.`));
+							}
+						},
+					});
+				},
+				__("Sync Now")
+			);
+		});
 	},
 
 	onload: function (frm) {
