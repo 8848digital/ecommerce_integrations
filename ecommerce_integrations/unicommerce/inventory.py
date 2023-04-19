8a05450ab7bfefbe56d3912d4e835645cda0bfd2
sonali8848 <115159164+sonali8848@users.noreply.github.com>
2023-04-19 15:56:07 +0530

    Update inventory.py

--- ecommerce_integrations/unicommerce/inventory.py
+++ ecommerce_integrations/unicommerce/inventory.py
@@ -26,7 +26,7 @@ def update_inventory_on_unicommerce(client=None, force=False):
 
 	force=True ignores the set frequency.
 	"""
-	settings = frappe.get_doc("Unicommerce Settings")
+	settings = frappe.get_cached_doc(SETTINGS_DOCTYPE)
 	if not settings.is_enabled() or not settings.enable_inventory_sync:
 		return
 
